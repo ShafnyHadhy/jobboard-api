d56f59170d3b05e3964d785955a58022e934d7c8
Shafny Hadhy <mohamedshafny02@gmail.com>
2026-07-23 23:04:24 +0530

    backend deployed url changed

--- jobboard-frontend/src/services/api.js
+++ jobboard-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: 'http://localhost:3000/api',
+    baseURL: 'https://jobboard-api-58qu.onrender.com/api',
 });
 
 // Automatically add the auth token to requests if it exists
